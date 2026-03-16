1bd1401c682bfa915b4e1947be5f7d79077ac9ae
FiraolKumsa <firaolkumsa911@gmail.com>
2026-03-16 16:10:33 -0400

    Create analysis.py

---
+++ src/analysis.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+income = pd.read_csv("data/income.csv")
+expenses = pd.read_csv("data/expenses.csv")
+
+income["salary"] = income["hours_worked"] * income["hourly_rate"]
+
+total_salary = income["salary"].sum()
+total_expense = expenses["amount"].sum()
+
+balance = total_salary - total_expense
+
+category_summary = expenses.groupby("category")["amount"].sum()
+
+print("Total Salary:", total_salary)
+print("Total Expenses:", total_expense)
+print("Balance:", balance)
+
+print("\nExpenses by Category")
+print(category_summary)
+
+report = pd.DataFrame({
+"salary":[total_salary],
+"expenses":[total_expense],
+"balance":[balance]
+})
+
+report.to_csv("reports/monthly_report.csv",index=False)
